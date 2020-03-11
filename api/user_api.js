e7f8c3222b43e90eb962d5a5853e2ead802fceb6
m.shahrokni <m.shahrokny@gmail.com>
2020-03-11 00:04:03 -0800

    # New dependencies were added to the project
    New dependencies were added to the project

--- api/user_api.js
+++ api/user_api.js
@@ -5,28 +5,32 @@ const bodyParser = require('body-parser');
 var api = express.Router();
 api.use(bodyParser.json());
 /*--------------------------------------------------*/
-api.post('/user', (req,res)=>{
-  
+api.post('/user', (req, res) => {
+
     let responseClass = require('../src/communication/entity/response');
- 
+
     let response = new responseClass();
-    
+
     let dateUtilModule = require('../src/util/date-util/date-util');
-   
-    let userValidationClass = require('../src/util/validation/user-validation'); 
-    let userValidation = new userValidationClass(); 
+
+    let userValidationClass = require('../src/util/validation/user-validation');
+    let userValidation = new userValidationClass();
     let errorMessages = userValidation.validateSignUpData(req.body);
-   
-    if(errorMessages!=null && errorMessages.length!=0){
- 
-       response.isSuccessful =false;
-       response.operationTimestamp = dateUtilModule.getCurrentDateTime();
-       response.setServerValidations(errorMessages);
-       res.status(200).end(JSON.stringify(response));
-       }
 
-       res.end('Nothing!');
- });
+    if (errorMessages != null && errorMessages.length != 0) {
+
+        response.isSuccessful = false;
+        response.operationTimestamp = dateUtilModule.getCurrentDateTime();
+        response.setServerValidations(errorMessages);
+        res.json({ response: response });
+        
+        //If you didn’t return, you’d continue on to the rest of the function and
+        //you’d send the request twice, and Express would start throwing nasty errors
+        return;
+    }
+
+    res.end('Nothing!');
+});
 
 
- module.exports = api;
+module.exports = api;
