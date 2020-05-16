52f2035ecdb9a6a95f48efb0daf966f7a1a5a2f1
vermin1990 <shahrokni.trivia@gmail.com>
2020-05-16 08:31:33 +0430

    XUI-05.16.2020.1

--- src/component/main-container/main-container.jsx
+++ src/component/main-container/main-container.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import './css/main-container.css';
+import {
+    globalMessageType,
+    defaultButtonText,
+    dataBlock
+} from '../../resource/text/global-message';
+
 export default class MainContainer extends React.Component {
 
     render() {
         return (
 
             <div id='mainContainer' className='absolute'>
-                 <React.Suspense fallback={<div>Loading...</div>}>
-                {this.generateInnerView(this.props.innerViewtId)}
+                <React.Suspense fallback={<div>Loading...</div>}>
+                    {this.generateInnerView(this.props.innerViewtId)}
                 </React.Suspense>
             </div>
         );
@@ -22,34 +28,45 @@ export default class MainContainer extends React.Component {
 
             case 'homePage':
                 TargetComponent = React.lazy(() => import('../homepage-view/homepage-view.jsx'));
-                finalComponent = <TargetComponent/>
+                finalComponent = <TargetComponent />
                 break;
             case 'mockTests':
-                TargetComponent = React.lazy(()=>import('../mocktests-view/mocktests.jsx'));
-                finalComponent = <TargetComponent/>
+                TargetComponent = React.lazy(() => import('../mocktests-view/mocktests.jsx'));
+                finalComponent = <TargetComponent />
                 break;
             case 'reports':
-                TargetComponent = React.lazy(()=>import('../reports-view/reports-view.jsx'));
-                finalComponent = <TargetComponent/>
+                TargetComponent = React.lazy(() => import('../reports-view/reports-view.jsx'));
+                finalComponent = <TargetComponent />
                 break;
             case 'account':
-                TargetComponent = React.lazy(()=>import('../account-view/account-view.jsx'));
-                finalComponent = <TargetComponent/>
+                TargetComponent = React.lazy(() => import('../account-view/account-view.jsx'));
+                finalComponent = <TargetComponent />
                 break;
             case 'wallet':
-                TargetComponent = React.lazy(()=>import('../wallet-view/wallet-view.jsx'));
-                finalComponent = <TargetComponent/>
+                TargetComponent = React.lazy(() => import('../wallet-view/wallet-view.jsx'));
+                finalComponent = <TargetComponent />
                 break;
             case 'messages':
-                TargetComponent = React.lazy(()=>import('../message-box-view/message-box-view.jsx'));
-                finalComponent = <TargetComponent/>
+                TargetComponent = React.lazy(() => import('../message-box-view/message-box-view.jsx'));
+                finalComponent = <TargetComponent />
                 break;
-            case 'about': 
-                TargetComponent = React.lazy(()=>import('../about-view/about-view.jsx'));
-                finalComponent = <TargetComponent/>
+            case 'about':
+                TargetComponent = React.lazy(() => import('../about-view/about-view.jsx'));
+                finalComponent = <TargetComponent />
+                break;
+            case 'languageLevel':
+                TargetComponent = React.lazy(() => import('../global-message-view/globa-message-view.jsx'));
+                finalComponent = <TargetComponent type={globalMessageType.info}
+                    buttonText={defaultButtonText.understand}
+                    title={dataBlock.d0000.title}
+                    text={dataBlock.d0000.text} />
+                break;
+            default:
+                TargetComponent = React.lazy(() => import('../homepage-view/homepage-view.jsx'));
+                finalComponent = <TargetComponent />
                 break;
         }
 
-       return finalComponent;
+        return finalComponent;
     }
 }
---
+++ src/resource/text/global-message.js
@@ -0,0 +1,23 @@
+const globalMessageType={
+    error:'Error',
+    warning:'Warning',
+    info:'Information',
+    check:'Check'
+}
+module.exports.globalMessageType=globalMessageType;
+/*----------------------------------------------*/
+const defaultButtonText = {
+    ok : 'Ok!',
+    agree : 'I agree',    
+    confirm: 'Confirm',
+    understand: 'I understand'
+}
+module.exports.defaultButtonText = defaultButtonText;
+/*----------------------------------------------*/
+const dataBlock = {
+    d0000:{
+        title:'You are about to participate in a language level test',
+        text:'test text'
+    }
+}
+module.exports.dataBlock = dataBlock;
