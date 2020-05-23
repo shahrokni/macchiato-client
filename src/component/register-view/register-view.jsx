538b841eb381e7f10a6f351e62e2d2f8fe5a2cfc
Shahrokni Mahmood <m.shahrokni@tosan.com>
2020-05-23 16:52:03 +0430

    XUI-05.23.2020.4

--- src/component/register-view/register-view.jsx
+++ src/component/register-view/register-view.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './css/register-view.css';
-
+import WelcomeBox from '../welcome-box/welcome-box';
+import LogoBox from '../logo-box/logo-box';
 export default class RegisterView extends React.Component{
 
       
     render(){
         return (
-            <h1>Register View!</h1>
+            <div className='fullSize absolute'>
+            <WelcomeBox text="Welcome to English Macchiato!"/>
+            <LogoBox />
+            </div>
         );     
     }
 }
