66c523d6bc305616e8f3f2eb96cc755703491abb
vermin1990 <shahrokni.trivia@gmail.com>
2020-06-20 13:48:52 +0430

    F-Front-06.08.2020-1.1

--- src/component/register-view/register-box.jsx
+++ src/component/register-view/register-box.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import './css/register-box.css';
+export default class RegisterBox extends React.Component {
 
-export default class RegisterBox extends React.Component{
-    
-    render(){
+    render() {
         return (
-            
-            null
+
+            <div className="registerBox">
+                <div className="registerBoxLog">
+                </div>
+                <div className="registerBoxSoftwareName">
+                    English Macchiato
+                </div>
+                <div className="registerBoxSolgan">
+                    Drink Your English Macchiato Now!
+                </div>
+                <div className="registerBoxBtnBox">
+                    <div id="registerBoxSignUpBtn" className="registerBoxBtn">
+                        Sign up
+                    </div>
+                    <div id="registerBoxSignInBtn" className="registerBoxBtn">
+                        Sign in
+                    </div>
+                </div>
+            </div>
         )
     }
 }
