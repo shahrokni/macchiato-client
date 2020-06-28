4527e6a1b6acb036228b8e2a51faac3146bfee15
vermin1990 <shahrokni.trivia@gmail.com>
2020-06-28 20:10:45 +0430

    F-Front-06.08.2020-1

--- src/component/register-view/register-view.jsx
+++ src/component/register-view/register-view.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 import './css/register-view.css';
 import WelcomeBox from '../welcome-box/welcome-box';
 import RegisterBox from './register-box.jsx';
+
 export default class RegisterView extends React.Component {
-
-
     render() {
         return (
 
             <div className='fullSize absolute'>
                 <WelcomeBox text="Welcome to English Macchiato!" />
-                <RegisterBox />
+                <RegisterBox linkClick ={this.props.linkClick}/>
             </div>
         );
     }
