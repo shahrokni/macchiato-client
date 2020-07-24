dae39edf732b2c6fa47b430c15c4c4e1de4c9f10
Seyed Mahmoud Shahrokni <shahrokni.trivia@gmail.com>
2020-07-24 01:15:49 -0700

    F-Front-06.28.2020-1.1

--- src/resource/text/common-global-messages.js
+++ src/resource/text/common-global-messages.js
@@ -1,12 +1,16 @@
-export default  class CommonGlobalMessages {
+import GlobalMessageViewModel from '../../component/global-message-view/view-model/global-message-view-model';
+import { GlobalMessageType } from '../../component/global-message-view/view-model/global-message-type';
+import ErrorMessage from './error-message';
+import { appGeneralInfo } from '../../setup-general-information';
+export default class CommonGlobalMessages {
 
     /* COMMUNICATION ERROR */
-    static getCommunicationErrorMessage(){
+    static getCommunicationErrorMessage() {
         let globalMessage = new GlobalMessageViewModel();
-        globalMessage.text = ErrorMessage.Err0000()+' Please try again!';
+        globalMessage.text = ErrorMessage.Err0000() + ' Please try again!';
         globalMessage.type = GlobalMessageType.Error;
         globalMessage.title = 'Something went wrong!'
-        globalMessage.redirect.link = '/'+appGeneralInfo.views.sigin;
+        globalMessage.redirect.link = '/' + appGeneralInfo.views.sigin;
         globalMessage.redirect.text = 'Try again!'
         return globalMessage;
     }
