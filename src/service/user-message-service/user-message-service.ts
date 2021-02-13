0a0a49a661020cfc4c7868b897150924fcc4c8cc
Shahrokni Mahmood <m.shahrokni@tosan.com>
2021-02-13 16:56:49 +0330

    F-Front-02.13.2021-1

--- src/service/user-message-service/user-message-service.ts
+++ src/service/user-message-service/user-message-service.ts
@@ -6,64 +6,103 @@ import rowMetaData from '../../entity/general-grid/row-meta-data';
 import UserMessage from '../../entity/user-message/user-message';
 import ErrorMessage from '../../resource/text/error-message';
 
-export default class UserMessageService implements  IListDataService {
+export default class UserMessageService implements IListDataService {
 
     dateUtil: any;
     constructor() {
         this.dateUtil = require('../../util/date-util/date-util');
-    }  
-
-    listData (filter: IFilter) : Promise<Response<rowMetaData[]>>{
-        return new Promise((resolve)=>{
-
-        });
-    }
-    countListData () : Promise<Response<number>>{
-        return new Promise((resolve)=>{
-
-        });
     }
 
-    getMessage(messageId:string):Promise<Response<UserMessage>> {
-        return new Promise((resolve)=>{
-            const response = new Response<UserMessage>();
+    listData(filter: IFilter): Promise<Response<rowMetaData[]>> {
+        return new Promise((resolve) => {
+            const response = new Response<rowMetaData[]>();
             response.isSuccessful = false;
             response.operationTimeClient = this.dateUtil.getCurrentDateTime();
             const restInstance = RestProvider.createInstance(RestProvider.getTimeoutDuration());
-            restInstance.get(`user_message_api/v1/message?ID=${messageId}`).then((res:any)=>{
+            restInstance.get('user_message_api/v1/message/listdata', { params: filter }).then((res: any) => {
                 let responseUtil = require('../../util/response-util/response-util');
-                let serverResponse = responseUtil.extractResponse(res) as Response<UserMessage>;
+                let serverResponse = responseUtil.extractResponse(res) as Response<UserMessage[]>;
                 if(serverResponse.isSuccessful){
+
+                }
+                else{
+
+                }
+            }).catch(() => {
+                response.isSuccessful = false;
+                response.clientValidations.push(ErrorMessage.Err0000().toString());
+                resolve(response);
+            });
+        });
+    }
+
+    countListData(): Promise<Response<number>> {
+        return new Promise((resolve) => {
+            const response = new Response<number>();
+            response.isSuccessful = false;
+            response.operationTimeClient = this.dateUtil.getCurrentDateTime();
+            const restInstance = RestProvider.createInstance(RestProvider.getTimeoutDuration());
+            restInstance.get('user_message_api/v1/message/countall').then((res: any) => {
+                let responseUtil = require('../../util/response-util/response-util');
+                let serverResponse = responseUtil.extractResponse(res) as Response<number>;
+                if (serverResponse.isSuccessful) {
                     response.isSuccessful = true;
                     response.outputJson = serverResponse.outputJson;
                     resolve(response);
                 }
-                else{
+                else {
                     response.isSuccessful = false;
                     resolve(response);
                 }
-            }).catch(()=>{
+            })
+                .catch((() => {
+                    response.isSuccessful = false;
+                    response.clientValidations.push(ErrorMessage.Err0000().toString());
+                    resolve(response);
+                }))
+        });
+    }
+
+    getMessage(messageId: string): Promise<Response<UserMessage>> {
+        return new Promise((resolve) => {
+            const response = new Response<UserMessage>();
+            response.isSuccessful = false;
+            response.operationTimeClient = this.dateUtil.getCurrentDateTime();
+            const restInstance = RestProvider.createInstance(RestProvider.getTimeoutDuration());
+            restInstance.get(`user_message_api/v1/message?ID=${messageId}`).then((res: any) => {
+                let responseUtil = require('../../util/response-util/response-util');
+                let serverResponse = responseUtil.extractResponse(res) as Response<UserMessage>;
+                if (serverResponse.isSuccessful) {
+                    response.isSuccessful = true;
+                    response.outputJson = serverResponse.outputJson;
+                    resolve(response);
+                }
+                else {
+                    response.isSuccessful = false;
+                    resolve(response);
+                }
+            }).catch(() => {
                 response.isSuccessful = false;
                 response.clientValidations.push(ErrorMessage.Err0000().toString());
                 resolve(response);
             })
-        });        
+        });
     }
 
-    deleteMessage(messageId:string):Promise<Response<void>> {    
-        return new Promise((resolve)=>{
+    deleteMessage(messageId: string): Promise<Response<void>> {
+        return new Promise((resolve) => {
             const response = new Response<void>();
             response.isSuccessful = false;
             response.operationTimeClient = this.dateUtil.getCurrentDateTime();
             const restInstance = RestProvider.createInstance(RestProvider.getTimeoutDuration());
-            restInstance.delete('user_message_api/v1/message', {params:{ messageId: messageId }}).then((res:any)=>{
+            restInstance.delete('user_message_api/v1/message', { params: { messageId: messageId } }).then((res: any) => {
                 let responseUtil = require('../../util/response-util/response-util');
                 let serverResponse = responseUtil.extractResponse(res) as Response<void>;
-                if(serverResponse.isSuccessful){
+                if (serverResponse.isSuccessful) {
                     response.isSuccessful = true;
                     resolve(response)
                 }
-                else{
+                else {
                     response.isSuccessful = false;
                     (serverResponse.serverValidations as string[]).forEach((serverError) => {
                         response.serverValidations.push(serverError);
@@ -71,7 +110,7 @@ export default class UserMessageService implements  IListDataService {
                     resolve(response);
                 }
 
-            }).catch(()=>{
+            }).catch(() => {
                 response.isSuccessful = false;
                 response.clientValidations.push(ErrorMessage.Err0000().toString());
                 resolve(response);
@@ -79,64 +118,64 @@ export default class UserMessageService implements  IListDataService {
         });
     }
 
-    setReadFlag(messageId:string):Promise<Response<void>> {      
-        return new Promise((resolve)=>{
+    setReadFlag(messageId: string): Promise<Response<void>> {
+        return new Promise((resolve) => {
             const response = new Response<void>();
             response.isSuccessful = false;
             response.operationTimeClient = this.dateUtil.getCurrentDateTime();
             const restInstance = RestProvider.createInstance(RestProvider.getTimeoutDuration());
             restInstance.put('user_message_api/v1/message/readFlag', { messageId: messageId })
-            .then((res:any)=>{
-                let responseUtil = require('../../util/response-util/response-util');
-                let serverResponse = responseUtil.extractResponse(res) as Response<void>;
-                if(serverResponse.isSuccessful){
-                    response.isSuccessful = true;
-                    resolve(response)
-                }
-                else{
+                .then((res: any) => {
+                    let responseUtil = require('../../util/response-util/response-util');
+                    let serverResponse = responseUtil.extractResponse(res) as Response<void>;
+                    if (serverResponse.isSuccessful) {
+                        response.isSuccessful = true;
+                        resolve(response)
+                    }
+                    else {
+                        response.isSuccessful = false;
+                        (serverResponse.serverValidations as string[]).forEach((serverError) => {
+                            response.serverValidations.push(serverError);
+                        });
+                        resolve(response);
+                    }
+                })
+                .catch(() => {
                     response.isSuccessful = false;
-                    (serverResponse.serverValidations as string[]).forEach((serverError) => {
-                        response.serverValidations.push(serverError);
-                    });
+                    response.clientValidations.push(ErrorMessage.Err0000().toString());
                     resolve(response);
-                }
-            })
-            .catch(()=>{
-                response.isSuccessful = false;
-                response.clientValidations.push(ErrorMessage.Err0000().toString());
-                resolve(response);
-            })
+                })
         });
     }
 
-    countUnreadMessages():Promise<Response<number>> {    
-        return new Promise((resolve)=>{
+    countUnreadMessages(): Promise<Response<number>> {
+        return new Promise((resolve) => {
             const response = new Response<number>();
             response.isSuccessful = false;
             response.operationTimeClient = this.dateUtil.getCurrentDateTime();
             const restInstance = RestProvider.createInstance(RestProvider.getTimeoutDuration());
-            restInstance.get('user_message_api/v1/unreadmessage/count')
-            .then((res:any)=>{
-                let responseUtil = require('../../util/response-util/response-util');
-                let serverResponse = responseUtil.extractResponse(res) as Response<number>;
-                if(serverResponse.isSuccessful){
-                    response.isSuccessful = true;
-                    response.outputJson = serverResponse.outputJson;
-                    resolve(response)
-                }
-                else{
+            restInstance.get('user_message_api/v1/message/countunread')
+                .then((res: any) => {
+                    let responseUtil = require('../../util/response-util/response-util');
+                    let serverResponse = responseUtil.extractResponse(res) as Response<number>;
+                    if (serverResponse.isSuccessful) {
+                        response.isSuccessful = true;
+                        response.outputJson = serverResponse.outputJson;
+                        resolve(response)
+                    }
+                    else {
+                        response.isSuccessful = false;
+                        (serverResponse.serverValidations as string[]).forEach((serverError) => {
+                            response.serverValidations.push(serverError);
+                        });
+                        resolve(response);
+                    }
+                })
+                .catch(() => {
                     response.isSuccessful = false;
-                    (serverResponse.serverValidations as string[]).forEach((serverError) => {
-                        response.serverValidations.push(serverError);
-                    });
+                    response.clientValidations.push(ErrorMessage.Err0000().toString());
                     resolve(response);
-                }
-            })
-            .catch(()=>{
-                response.isSuccessful = false;
-                response.clientValidations.push(ErrorMessage.Err0000().toString());
-                resolve(response);
-            })
+                })
         });
-    }    
+    }
 }
